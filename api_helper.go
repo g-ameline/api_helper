c5a55aa7497968c902da49627de66b0d20c49e1c
gameline <gameline_kood>
2023-09-17 23:13:50 +0300

    improved

--- api_helper.go
+++ api_helper.go
@@ -14,16 +14,8 @@ import (
 )
 
 const content_type_json = "application/json"
-
-func Post_data_to_url[data_type any](a_url string, data data_type) (response *http.Response, err error) {
-	data_json, err := json.Marshal(data)
-	if err != nil {
-		return response, err
-	}
-	data_reader := bytes.NewBuffer(data_json)
-	response, err = http.Post(a_url, content_type_json, data_reader)
-	return response, err
-}
+const content_type_form = "application/x-www-form-urlencoded"
+const content_type_multipart = "multipart/form-data"
 
 func new_request(method, a_url string, body_reader io.Reader) (*http.Request, error) {
 	return http.NewRequest(method, a_url, body_reader)
@@ -47,53 +39,89 @@ func Add_data(request *http.Request) {
 
 }
 
-func PostForm_data_with_cookie_to_url(a_url string, data map[string]any, cookie_data map[string]string) (response *http.Response, err error) {
-	post_form := url.Values{}
-	for key, value := range data {
-		switch value.(type) {
-		case string:
-			post_form.Set(key, value.(string))
-		case []string:
-			post_form[key] = value.([]string)
-		default:
-			err = fmt.Errorf(fmt.Sprintf("need string or []string but got type %T", value))
-		}
-	}
+func Post_json_data_to_url[data_type any](a_url string, data data_type) (response *http.Response, err error) {
+	data_json, err := json.Marshal(data)
 	if err != nil {
 		return response, err
 	}
-	data_reader := strings.NewReader(post_form.Encode())
-	request, err := http.NewRequest(http.MethodPost, a_url, data_reader)
-	Add_cookie(request, cookie_data["Name"], cookie_data["Value"])
-	client := &http.Client{}
-	return client.Do(request)
+	data_reader := bytes.NewBuffer(data_json)
+	response, err = http.Post(a_url, content_type_json, data_reader)
+	return response, err
 }
-
-func PostForm_data_to_url(a_url string, data map[string]any) (response *http.Response, err error) {
+func Post_form_data_to_url(a_url string, data map[string]any) (response *http.Response, err error) {
 	post_form := url.Values{}
 	for key, value := range data {
 		switch value.(type) {
 		case string:
 			post_form.Set(key, value.(string))
 		case []string:
-			post_form[key] = value.([]string)
+			list, _ := value.([]string)
+			post_form[key] = list
+			// post_form[key] = value.([]string)
 		default:
 			err = fmt.Errorf(fmt.Sprintf("need string or []string but got type %T", value))
 		}
 	}
+	fmt.Println("just before posting", post_form)
 	if err != nil {
 		return response, err
 	}
 	response, err = http.PostForm(a_url, post_form)
 	return response, err
 }
-func Get_data_from_request[data_type any](request *http.Request) (data_type, error) {
+
+func Post_form_data_with_cookie_to_url(a_url string, data map[string]any, cookie_data map[string]string) (response *http.Response, err error) {
+	// var b bytes.Buffer
+	post_form := url.Values{}
+	for key, value := range data {
+		switch value.(type) {
+		case string:
+			post_form.Set(key, value.(string))
+		case []string:
+			post_form[key] = value.([]string)
+		default:
+			err = fmt.Errorf(fmt.Sprintf("need string or []string but got type %T", value))
+		}
+	}
+	if err != nil {
+		return response, err
+	}
+	fmt.Println("form", post_form)
+	data_reader := strings.NewReader(post_form.Encode())
+	request, err := http.NewRequest("POST", a_url, data_reader)
+	request.PostForm = post_form
+	request.Header.Set("Content-Type", content_type_form)
+	Add_cookie(request, cookie_data["Name"], cookie_data["Value"])
+	return http.DefaultClient.Do(request)
+}
+func Post_multipart_data_to_url(a_url string, data map[string]any) (response *http.Response, err error) {
+	body_buffer := new(bytes.Buffer)
+	writer := multipart.NewWriter(body_buffer)
+	fmt.Println("data", data)
+	for key, value := range data {
+		switch value.(type) {
+		case string:
+			err = writer.WriteField(key, value.(string))
+		case []string:
+			err = writer.WriteField(key, fmt.Sprintf("%v", value))
+		default:
+			panic("cant make it string")
+		}
+		if err != nil {
+			panic("failed")
+		}
+	}
+	writer.Close()
+	return http.Post(a_url, writer.FormDataContentType(), body_buffer)
+}
+
+func Get_data_from_request_json[data_type any](request *http.Request) (data_type, error) {
 	decoder := json.NewDecoder(request.Body)
 	var data data_type
 	err := decoder.Decode(&data)
 	return data, err
 }
-func Get_data_from_response[data_type any](request *http.Response) (data_type, error) {
+func Get_data_from_response_json[data_type any](request *http.Response) (data_type, error) {
 	decoder := json.NewDecoder(request.Body)
 	var data data_type
 	err := decoder.Decode(&data)
@@ -134,11 +162,13 @@ func Get_cookie_data_from_response(response *http.Response, cookie_name string) 
 	return cookie_map, err
 }
 
-func Get_data_from_post_form_from_request(request *http.Request) (map[string]any, error) {
+func Get_data_from_request_form(request *http.Request) (map[string]any, error) {
 	err := request.ParseForm()
+	fmt.Println("post form fromt requst", request.PostForm)
 	data := map[string]any{}
 	if err == nil {
 		for k, v := range request.PostForm {
+			fmt.Println("into form", k, v, len(v))
 			switch len(v) {
 			case 1:
 				data[k] = v[0]
@@ -200,16 +230,11 @@ func Post_saved_file_to_url(a_url string, file_path, file_type string) (response
 	}
 	request, err := http.NewRequest("POST", a_url, body_buffer)
 	request.Header.Set("Content-Type", writer.FormDataContentType())
-	client := &http.Client{}
-	response, err = client.Do(request)
-	if err != nil {
-		return response, err
-	}
-	defer response.Body.Close()
-	return response, err
+	// defer body_buffer.Close()
+	return http.DefaultClient.Do(request)
 }
 
-func Get_files_from_post_form(r *http.Request) (files map[string]multipart.File, err error) {
+func Get_files_from_request_multipart(r *http.Request) (files map[string]multipart.File, err error) {
 	files = map[string]multipart.File{}
 	err = r.ParseMultipartForm(32 << 10)
 	if err != nil {
@@ -223,6 +248,50 @@ func Get_files_from_post_form(r *http.Request) (files map[string]multipart.File,
 	}
 	return files, err
 }
+func Get_data_and_files_from_request_multipart(r *http.Request) (data map[string]any, files map[string]multipart.File, err error) {
+	data = map[string]any{}
+	files = map[string]multipart.File{}
+	err = r.ParseMultipartForm(32 << 10)
+	if err != nil {
+		return data, files, err
+	}
+	for k, v := range r.MultipartForm.File {
+		file, err := v[0].Open()
+		if err == nil {
+			files[k] = file
+		}
+	}
+	for k, v := range r.MultipartForm.Value {
+		switch len(v) {
+		case 1:
+			data[k] = v[0]
+		case 0:
+			continue
+		default:
+			data[k] = v
+		}
+	}
+	return data, files, err
+}
+func Get_data_from_request_multipart(r *http.Request) (data map[string]any, err error) {
+	data = map[string]any{}
+	err = r.ParseMultipartForm(32 << 10)
+	if err != nil {
+		return data, err
+	}
+	fmt.Println("form of multipart", r.MultipartForm)
+	for k, v := range r.MultipartForm.Value {
+		switch len(v) {
+		case 1:
+			data[k] = v[0]
+		case 0:
+			continue
+		default:
+			data[k] = v
+		}
+	}
+	return data, err
+}
 func decide_on_response(response *http.Response) (err error) {
 	switch response.StatusCode {
 	case 200:
@@ -233,53 +302,58 @@ func decide_on_response(response *http.Response) (err error) {
 	}
 	return err
 }
-func Post_all_kind_of_data(client *http.Client, url string, data map[string]io.Reader) (response *http.Response, err error) {
+func Post_data_and_files(a_url string, data map[string]any, files map[string]multipart.File) (response *http.Response, err error) {
 	// Prepare a form that you will submit to that URL.
-	var b bytes.Buffer
-	w := multipart.NewWriter(&b)
-	for key, r := range data {
-		var fw io.Writer
-		if x, ok := r.(io.Closer); ok {
-			defer x.Close()
+	body_buffer := new(bytes.Buffer)
+	writer := multipart.NewWriter(body_buffer)
+	for key, value := range data {
+		switch value.(type) {
+		case string:
+			err = writer.WriteField(key, value.(string))
+		case []string:
+			err = writer.WriteField(key, fmt.Sprintf("%v", value))
+		default:
+			panic("cant make it string")
 		}
-		// Add an image file
-		if x, ok := r.(*os.File); ok {
-			if fw, err = w.CreateFormFile(key, x.Name()); err != nil {
-				return response, err
-			}
-		} else {
-			// Add other fields
-			if fw, err = w.CreateFormField(key); err != nil {
-				return response, err
-			}
+		if err != nil {
+			panic("failed")
 		}
-		if _, err = io.Copy(fw, r); err != nil {
+	}
+	// prepare writing files
+	for file_name, file := range files {
+		part, err := writer.CreateFormFile("File", file_name)
+		if err != nil {
+			return response, err
+		}
+		_, err = io.Copy(part, file)
+		defer file.Close()
+		if err != nil {
 			return response, err
 		}
 
 	}
 	// If you don't close it, your request will be missing the terminating boundary.
-	w.Close()
-	// Now that you have a form, you can submit it to your handler.
-	request, err := http.NewRequest("POST", url, &b)
-	if err != nil {
-		return
-	}
+	err = writer.Close()
 	// Don't forget to set the content type, this will contain the boundary.
-	request.Header.Set("Content-Type", w.FormDataContentType())
-	// Submit the request
-	return client.Do(request)
+	request, err := http.NewRequest("POST", a_url, body_buffer)
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+	// defer response.Body.Close()
+	return http.DefaultClient.Do(request)
 }
-
-// type R interface {
-// 	*http.Request | *http.Response
-// }
-
-// type R interface {
-// 	what() bool
-// // 	// Header  http.Header
-// // 	// Body io.ReadCloser
-// // 	// ContentLength int64
-// 	Close() bool
-// // 	// Cookie(string)(*http.Cookie,error)
-// }
+func Get_data_from_request(responder http.ResponseWriter, request *http.Request) (data map[string]any, err error) {
+	data = map[string]any{}
+	fmt.Println("content type ", request.Header.Get("content-type"))
+	only_content_type := strings.Split(request.Header.Get("Content-Type"), ";")[0]
+	switch only_content_type {
+	case content_type_json:
+		data, err = Get_data_from_request_json[map[string]any](request)
+		fmt.Println("body", data, err)
+	case content_type_form:
+		data, err = Get_data_from_request_form(request)
+		fmt.Println("post form", data, err)
+	case content_type_multipart:
+		data, data["files"], err = Get_data_and_files_from_request_multipart(request)
+		fmt.Println("multipart", data, err)
+	}
+	return data, err
+}
--- api_helper_test.go
+++ api_helper_test.go
@@ -3,7 +3,9 @@ package api_helper
 import (
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -28,6 +30,8 @@ func Test(t *testing.T) {
 	server.HandleFunc(root+"data_as_post_form", print_data_from_post_form_from_request)
 	server.HandleFunc(root+"whatever", print_data_from_whatever_from_request)
 	server.HandleFunc(root+"a_file", check_if_file)
+	server.HandleFunc(root+"multipart", print_data_from_request_multipart)
+	server.HandleFunc(root+"data_files", print_data_and_files_from_request_multipart)
 
 	// next we send stuff to the server
 
@@ -35,36 +39,39 @@ func Test(t *testing.T) {
 	some_data["patate"] = []int{1, 2, 3, 4, 6}
 	some_data["courgette"] = "rododindron"
 	println("\nsend data through request body")
-	response, err := Post_data_to_url(home_url+"data_in_body", some_data)
+	response, err := Post_json_data_to_url(home_url+"data_in_body", some_data)
 	fmt.Println("response from sending data", response.StatusCode)
 	print_response_body(response)
 	fmt.Println("error", err)
 	just_a_word := "voila"
-	response, err = Post_data_to_url(home_url+"data_in_body", just_a_word)
+	response, err = Post_json_data_to_url(home_url+"data_in_body", just_a_word)
 	fmt.Println("response from sending data", response.StatusCode)
 	print_response_body(response)
 	fmt.Println("error", err)
 	println("\nsend data as post form")
-	response, err = PostForm_data_to_url(home_url+"data_as_post_form", some_data)
+	response, err = Post_form_data_to_url(home_url+"data_as_post_form", some_data)
 	// fmt.Println("response from sending data", response.StatusCode)
 	fmt.Println("error", err)
+	println("\nsend data through request body as multipart")
 	new_data := map[string]any{}
 	new_data["cacahouette"] = "banane"
-	new_data["rutabaga"] = []string{"2,1,3,carotte"}
-	response, err = PostForm_data_to_url(home_url+"data_as_post_form", new_data)
+	new_data["rutabaga"] = []string{"2", "1", "3", "carotte"}
+	response, err = Post_multipart_data_to_url(home_url+"multipart", new_data)
+	fmt.Println("error", err)
+	response, err = Post_form_data_to_url(home_url+"data_as_post_form", new_data)
 	fmt.Println("response from sending data", response.StatusCode)
 	print_response_body(response)
 	fmt.Println("error", err)
 	println("\nsend data in whatever way")
 	fmt.Println("data before anything", new_data)
 	println("post form")
-	response, err = PostForm_data_to_url(home_url+"whatever", new_data)
+	response, err = Post_form_data_to_url(home_url+"whatever", new_data)
 	fmt.Println("response from sending data", response.StatusCode)
 	print_response_body(response)
 	fmt.Println("error", err)
 	println("")
 	println("post body")
-	response, err = Post_data_to_url(home_url+"whatever", new_data)
+	response, err = Post_json_data_to_url(home_url+"whatever", new_data)
 	fmt.Println("response from sending data", response.StatusCode)
 	print_response_body(response)
 	fmt.Println("error", err)
@@ -75,11 +82,48 @@ func Test(t *testing.T) {
 	fmt.Println("error", err)
 	file_to_send, err := Open_saved_file("./doc.jpeg")
 	response, err = Post_file_to_url(home_url+"a_file", file_to_send, "image")
+	// file_to_send.Close()
 	fmt.Println("response from sending file", response.StatusCode)
 	print_response_body(response)
 	fmt.Println("error", err)
 	println("\nsend a file in post form get in with whatever")
 	response, err = Post_saved_file_to_url(home_url+"whatever", "./doc.jpeg", "image")
+	fmt.Println("response from sending file", response.StatusCode)
+	print_response_body(response)
+	fmt.Println("error", err)
+	println("\nsend post form and cookie")
+	cookie_data := map[string]string{}
+	cookie_data["Value"] = "TAPOUERE!"
+	cookie_data["Name"] = "PUREE!"
+	response, err = Post_form_data_with_cookie_to_url(home_url+"data_as_post_form", new_data, cookie_data)
+	fmt.Println("response from sending data", response.StatusCode)
+	print_response_body(response)
+	fmt.Println("error", err)
+	println("\nsend stuff however and get it anyever")
+	response, err = Post_json_data_to_url(home_url+"whatever", new_data)
+	fmt.Println("response from sending data", response.StatusCode)
+	print_response_body(response)
+	fmt.Println("error", err)
+	response, err = Post_form_data_to_url(home_url+"whatever", new_data)
+	fmt.Println("response from sending data", response.StatusCode)
+	print_response_body(response)
+	fmt.Println("error", err)
+	response, err = Post_multipart_data_to_url(home_url+"whatever", new_data)
+	fmt.Println("response from sending data", response.StatusCode)
+	print_response_body(response)
+	fmt.Println("error", err)
+	println("\nsend a file in post with form data")
+	file_to_send, err = Open_saved_file("./doc.jpeg")
+	files_to_send := map[string]multipart.File{}
+	files_to_send["doc"] = file_to_send
+	response, err = Post_data_and_files(home_url+"data_files", new_data, files_to_send)
+	// file_to_send.Close()
+	fmt.Println("response from sending file", response.StatusCode)
+	print_response_body(response)
+	fmt.Println("error", err)
+	files_to_send = map[string]multipart.File{}
+	response, err = Post_data_and_files(home_url+"data_files", new_data, files_to_send)
+	// file_to_send.Close()
 	fmt.Println("response from sending file", response.StatusCode)
 	print_response_body(response)
 	fmt.Println("error", err)
@@ -93,43 +137,75 @@ func print_response_body(response *http.Response) {
 
 }
 func print_data_from_request(responder http.ResponseWriter, request *http.Request) {
-	data, err := Get_data_from_request[any](request)
+	data, err := Get_data_from_request_json[any](request)
 	fmt.Println("data received from request", data)
+	cookies := request.Cookies()
+	if len(cookies) > 0 {
+		fmt.Println("cookies", cookies)
+	}
+	fmt.Printf("of type %T\n", data)
+	fmt.Println("the error", err)
+	responder.Write([]byte("putain prout"))
+}
+func print_data_from_request_multipart(responder http.ResponseWriter, request *http.Request) {
+	data, err := Get_data_from_request_multipart(request)
+	cookies := request.Cookies()
+	if len(cookies) > 0 {
+		fmt.Println("cookies", cookies)
+	}
 	fmt.Printf("of type %T\n", data)
 	fmt.Println("the error", err)
 	responder.Write([]byte("putain prout"))
 }
 func print_data_from_post_form_from_request(responder http.ResponseWriter, request *http.Request) {
-	data, err := Get_data_from_post_form_from_request(request)
+	data, err := Get_data_from_request_form(request)
 	fmt.Println("data received from postform from request", data)
+	cookies := request.Cookies()
+	if len(cookies) > 0 {
+		fmt.Println("cookies", cookies)
+	}
 	fmt.Printf("of type %T\n", data)
 	fmt.Println("the error", err)
 	responder.Write([]byte("putain dieu"))
 }
+func print_data_and_files_from_request_multipart(responder http.ResponseWriter, request *http.Request) {
+	data, files, err := Get_data_and_files_from_request_multipart(request)
+	fmt.Println("data received from multipart from request", data)
+	fmt.Println("file received from multipart from request", files)
+	cookies := request.Cookies()
+	if len(cookies) > 0 {
+		fmt.Println("cookies", cookies)
+	}
+	fmt.Printf("of type %T\n", data)
+	fmt.Printf("of type %T\n", data)
+	fmt.Println("the error", err)
+	responder.Write([]byte("putain prout"))
+}
 func print_data_from_whatever_from_request(responder http.ResponseWriter, request *http.Request) {
-	data_two, err := Get_data_from_post_form_from_request(request)
-	fmt.Println("post form", data_two, err)
-	data_three, err := Get_files_from_post_form(request)
-	fmt.Println("file", data_three, err)
-	data_one, err := Get_data_from_request[map[string]any](request)
-	fmt.Println("body", data_one, err)
 	data := map[string]any{}
-	for k, v := range data_one {
-		data[k] = v
+	err := *new(error)
+	fmt.Println("content type ", request.Header.Get("content-type"))
+	only_content_type := strings.Split(request.Header.Get("Content-Type"), ";")[0]
+	switch only_content_type {
+	case content_type_json:
+		data, err = Get_data_from_request_json[map[string]any](request)
+		fmt.Println("body", data, err)
+	case content_type_form:
+		data, err = Get_data_from_request_form(request)
+		fmt.Println("post form", data, err)
+	case content_type_multipart:
+		data, data["files"], err = Get_data_and_files_from_request_multipart(request)
+		fmt.Println("multipart", data, err)
+		// data, err := Get_files_from_request_multipart(request)
+		// fmt.Println("file", data, err)
 	}
-	for k, v := range data_two {
-		data[k] = v
-	}
-	for k, v := range data_three {
-		data[k] = v
-	}
-	fmt.Println("data received from postform from request", data)
+	fmt.Println("data received from request", data)
 	fmt.Printf("of type %T\n", data)
 	fmt.Println("the error", err)
 	responder.Write([]byte("putain con"))
 }
 func check_if_file(responder http.ResponseWriter, request *http.Request) {
-	data, err := Get_files_from_post_form(request)
+	data, err := Get_files_from_request_multipart(request)
 	fmt.Println("data received from postform from request", data)
 	fmt.Printf("of type %T\n", data)
 	fmt.Println("the error", err)
